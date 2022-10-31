166bebd077ab28011d6a4557316f9d7e6c18f543
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-10-31 16:30:21 +0700

    cors

--- pages/api/cors.js
+++ pages/api/cors.js
@@ -1,6 +1,6 @@
 import NextCors from 'nextjs-cors';
 
-async function handler(req, res) {
+export async function handler(req, res) {
    await NextCors(req, res, {
       methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
       origin: 'http://127.0.0.1:8090/',
@@ -9,5 +9,7 @@ async function handler(req, res) {
    });
 
    // Rest of the API logic
+   res.header("Access-Control-Allow-Origin", origin);
+   res.header("Access-Control-Allow-Credentials", true);
    res.json({ message: 'Hello NextJs Cors!' });
 }
